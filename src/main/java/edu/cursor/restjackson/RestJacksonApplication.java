979486c912d12a379ff6e00049103afed14fcb7f
BogdanShulga <bogdan.shulga.90@gmail.com>
2019-08-07 21:14:38 +0300

    master: initial commit

---
+++ src/main/java/edu/cursor/restjackson/RestJacksonApplication.java
@@ -0,0 +1,13 @@
+package edu.cursor.restjackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJacksonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestJacksonApplication.class, args);
+    }
+
+}
